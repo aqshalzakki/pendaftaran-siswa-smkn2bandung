31bc1cba1fc32f5805a1ea5c7d7f33c5e6218f63
aqshalzakki <kecoatempur2@gmail.com>
2019-11-22 20:57:52 +0700

    sedikit perubahan

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -86,7 +86,7 @@
     	<div class="container">
     		<div class="row d-md-flex justify-content-center">
     			<div class="col-md-9 about-video text-center">
-    				<h2 class="ftco-animate">Sekolah yang menghasilkan generasi penerus bangsa.</h2>
+   				<h2 class="ftco-animate">Sekolah yang menghasilkan generasi penerus bangsa yang siap bekerja maupun berwirausaha.</h2>
     				<div class="video d-flex justify-content-center">
     					{{-- VIDEO TENTANG SMKN 2 BANDUNG --}}
     					<a href="https://www.youtube.com/watch?v=e50wGoHhbz0" class="button popup-vimeo d-flex justify-content-center align-items-center"><span class="ion-ios-play"></span></a>
@@ -106,32 +106,32 @@
 		          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
 		            <div class="block-18 text-center">
 		              <div class="text">
-		                <strong class="number" data-number="10300">0</strong>
-		                <span>Satisfied Students</span>
+		                <strong class="number" data-number="4000">0</strong>
+		                <span>Jumlah Kuota Pendaftaran</span>
 		              </div>
 		            </div>
 		          </div>
 		          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
 		            <div class="block-18 text-center">
 		              <div class="text">
-		                <strong class="number" data-number="7896">0</strong>
-		                <span>Courses Completed</span>
+		                <strong class="number" data-number="2304">0</strong>
+		                <span>Jumlah Pendaftar</span>
 		              </div>
 		            </div>
 		          </div>
 		          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
 		            <div class="block-18 text-center">
 		              <div class="text">
-		                <strong class="number" data-number="400">0</strong>
-		                <span>Experts Advisors</span>
+		                <strong class="number" data-number="">0</strong>
+		                <span>Peserta yang diterima</span>
 		              </div>
 		            </div>
 		          </div>
 		          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
 		            <div class="block-18 text-center">
 		              <div class="text">
-		                <strong class="number" data-number="200">0</strong>
-		                <span>Schools</span>
+		                <strong class="number" data-number="">0</strong>
+		                <span>Peserta yang ditolak</span>
 		              </div>
 		            </div>
 		          </div>
@@ -141,83 +141,11 @@
     	</div>
     </section>
 
-    <section class="ftco-section testimony-section">
+    <section class="ftco-section">
       <div class="container">
       	<div class="row justify-content-center mb-5 pb-3">
           <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">What Our Student Says</h2>
-          </div>
-        </div>
-        <div class="row">
-        	<div class="col-md-12 ftco-animate">
-            <div class="carousel-testimony owl-carousel">
-              <div class="item">
-                <div class="testimony-wrap text-center">
-                  <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
-                    <span class="quote d-flex align-items-center justify-content-center">
-                      <i class="icon-quote-left"></i>
-                    </span>
-                  </div>
-                  <div class="text">
-                    <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="name">Dennis Green</p>
-                    <span class="position">CSE Student</span>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimony-wrap text-center">
-                  <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
-                    <span class="quote d-flex align-items-center justify-content-center">
-                      <i class="icon-quote-left"></i>
-                    </span>
-                  </div>
-                  <div class="text">
-                    <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="name">Dennis Green</p>
-                    <span class="position">Math Student</span>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimony-wrap text-center">
-                  <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
-                    <span class="quote d-flex align-items-center justify-content-center">
-                      <i class="icon-quote-left"></i>
-                    </span>
-                  </div>
-                  <div class="text">
-                    <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="name">Dennis Green</p>
-                    <span class="position">Science Students</span>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimony-wrap text-center">
-                  <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
-                    <span class="quote d-flex align-items-center justify-content-center">
-                      <i class="icon-quote-left"></i>
-                    </span>
-                  </div>
-                  <div class="text">
-                    <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="name">Dennis Green</p>
-                    <span class="position">English Student</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="ftco-section bg-light">
-      <div class="container">
-      	<div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Our Experience Advisor</h2>
+            <h1 class="mb-4">Kepala Sekolah beserta Wakilnya</h1>
           </div>
         </div>
         <div class="row">
@@ -226,8 +154,8 @@
         			<div class="d-flex mb-4">
         				<div class="img" style="background-image: url(images/person_1.jpg);"></div>
         				<div class="info ml-4">
-        					<h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-        					<span class="position">CSE Teacher</span>
+        					<h3><a href="teacher-single.html">Sukarna S.Pd</a></h3>
+        					<span class="position">Wakil Kepala Sekolah</span>
         					<p class="ftco-social d-flex">
 		                <a href="#" class="d-flex justify-content-center align-items-center"><span class="icon-twitter"></span></a>
 		                <a href="#" class="d-flex justify-content-center align-items-center"><span class="icon-facebook"></span></a>
@@ -245,8 +173,8 @@
         			<div class="d-flex mb-4">
         				<div class="img" style="background-image: url(images/person_2.jpg);"></div>
         				<div class="info ml-4">
-        					<h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-        					<span class="position">CSE Teacher</span>
+        					<h3><a href="teacher-single.html">Tatang Gunawan S.Pd</a></h3>
+        					<span class="position">Kepala Sekolah</span>
         					<p class="ftco-social d-flex">
 		                <a href="#" class="d-flex justify-content-center align-items-center"><span class="icon-twitter"></span></a>
 		                <a href="#" class="d-flex justify-content-center align-items-center"><span class="icon-facebook"></span></a>
@@ -304,7 +232,7 @@
     	<div class="container">
     		<div class="row justify-content-center mb-5 pb-3">
           <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Our Courses</h2>
+            <h2 class="mb-4">Daftar Kejuruan</h2>
           </div>
         </div>
     		<div class="row">
@@ -312,10 +240,10 @@
     				<div class="course align-self-stretch">
     					<a href="#" class="img" style="background-image: url(images/course-1.jpg)"></a>
     					<div class="text p-4">
-    						<p class="category"><span>English</span></p>
-    						<h3 class="mb-3"><a href="#">English for Tommorow</a></h3>
+    						<p class="category"><span>Teknologi</span></p>
+    						<h3 class="mb-3"><a href="#">Teknik Komputer dan Informatika</a></h3>
     						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name</p>
-    						<p><a href="#" class="btn btn-primary">Enroll now!</a></p>
+    						<p><a href="#" class="btn btn-primary">Lihat detail...</a></p>
     					</div>
     				</div>
     			</div>
@@ -323,10 +251,10 @@
     				<div class="course align-self-stretch">
     					<a href="#" class="img" style="background-image: url(images/course-2.jpg)"></a>
     					<div class="text p-4">
-    						<p class="category"><span>Science</span></p>
-    						<h3 class="mb-3"><a href="#">Computer Engineering</a></h3>
+    						<p class="category"><span>Permesinan</span></p>
+    						<h3 class="mb-3"><a href="#">Teknik Mesin</a></h3>
     						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name</p>
-    						<p><a href="#" class="btn btn-primary">Enroll now!</a></p>
+    						<p><a href="#" class="btn btn-primary">Lihat detail...</a></p>
     					</div>
     				</div>
     			</div>
@@ -334,99 +262,38 @@
     				<div class="course align-self-stretch">
     					<a href="#" class="img" style="background-image: url(images/course-3.jpg)"></a>
     					<div class="text p-4">
-    						<p class="category"><span>Business</span></p>
-    						<h3 class="mb-3"><a href="#">Business Studies</a></h3>
+    						<p class="category"><span>Kesenian</span></p>
+    						<h3 class="mb-3"><a href="#">Seni Rupa</a></h3>
     						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name</p>
-    						<p><a href="#" class="btn btn-primary">Enroll now!</a></p>
+    						<p><a href="#" class="btn btn-primary">Lihat detail...</a></p>
     					</div>
     				</div>
     			</div>
     		</div>
-    		<div class="row justify-content-center mt-5">
-        	<div class="col-md-10 ftco-animate">
-        		<p><strong>When she reached</strong> the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p>
-        		<p><span>Just Browsing?</span><a href="course.html"> View All Courses</a></p>
-        	</div>
-        </div>
     	</div>
     </section>
 
+    
     <section class="ftco-section bg-light">
       <div class="container">
         <div class="row justify-content-center mb-5 pb-3">
           <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Recent from blog</h2>
-          </div>
-        </div>
-        <div class="row d-flex">
-          <div class="col-md-4 d-flex ftco-animate">
-          	<div class="blog-entry align-self-stretch">
-              <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');">
-              </a>
-              <div class="text p-4 d-block">
-              	<div class="meta mb-3">
-                  <div><a href="#">August 12, 2018</a></div>
-                  <div><a href="#">Admin</a></div>
-                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                </div>
-                <h3 class="heading mt-3"><a href="#">How to standout at start of your UX Career</a></h3>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 d-flex ftco-animate">
-          	<div class="blog-entry align-self-stretch">
-              <a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');">
-              </a>
-              <div class="text p-4 d-block">
-              	<div class="meta mb-3">
-                  <div><a href="#">August 12, 2018</a></div>
-                  <div><a href="#">Admin</a></div>
-                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                </div>
-                <h3 class="heading mt-3"><a href="#">How to standout at start of your UX Career</a></h3>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 d-flex ftco-animate">
-          	<div class="blog-entry align-self-stretch">
-              <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
-              </a>
-              <div class="text p-4 d-block">
-              	<div class="meta mb-3">
-                  <div><a href="#">August 12, 2018</a></div>
-                  <div><a href="#">Admin</a></div>
-                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                </div>
-                <h3 class="heading mt-3"><a href="#">How to standout at start of your UX Career</a></h3>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="ftco-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Our Latest Events</h2>
+            <h2 class="mb-4">Berita Terbaru</h2>
           </div>
         </div>
         <div class="row">
         	<div class="col-md-4 d-flex ftco-animate">
           	<div class="blog-entry align-self-stretch">
-              <a href="blog-single.html" class="block-20" style="background-image: url('images/event-1.jpg');">
+              <a href="blog-single.html" class="block-20" style="background-image: url({{ asset('images/kegiatan/jerawat.jpg') }});">
               </a>
               <div class="text p-4 d-block">
               	<div class="meta mb-3">
-                  <div><a href="#">Sep. 10, 2018</a></div>
+                  <div><a href="#">Sep. 10, 2019</a></div>
                   <div><a href="#">Admin</a></div>
                   <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
                 </div>
-                <h3 class="heading mb-4"><a href="#">Intern Bootcamp Meetup 2018</a></h3>
+                {{-- <h3 class="heading mb-4"><a href="#">Jumlah pendaftar PPDB mencapai 20.000 peserta</a></h3> --}}
+                <h3 class="heading mb-4"><a href="#">WOW ternyata inilah 10 penyebab tumbuhnya jerawat pada bagian kaki... No.7 mencengangkan!!</a></h3>
                 <p class="time-loc"><span class="mr-2"><i class="icon-clock-o"></i> 10:30AM-03:30PM</span> <span><i class="icon-map-o"></i> Venue Main Campus</span></p>
                 <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
                 <p><a href="event.html">Join Event <i class="ion-ios-arrow-forward"></i></a></p>
@@ -435,7 +302,7 @@
           </div>
           <div class="col-md-4 d-flex ftco-animate">
           	<div class="blog-entry d-flex align-self-stretch flex-column-reverse">
-              <a href="blog-single.html" class="block-20" style="background-image: url('images/event-2.jpg');">
+              <a href="blog-single.html" class="block-20" style="background-image: url({{ asset('images/kegiatan/oura.jpg') }});">
               </a>
               <div class="text p-4 d-block">
               	<div class="meta mb-3">
@@ -443,7 +310,7 @@
                   <div><a href="#">Admin</a></div>
                   <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
                 </div>
-                <h3 class="heading mb-4"><a href="#">Intern Bootcamp Meetup 2018</a></h3>
+                <h3 class="heading mb-4"><a href="#">Evos Oura pick estes? tencent bercanda?</a></h3>
                 <p class="time-loc"><span class="mr-2"><i class="icon-clock-o"></i> 10:30AM-03:30PM</span> <span><i class="icon-map-o"></i> Venue Main Campus</span></p>
                 <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
                 <p><a href="event.html">Join Event <i class="ion-ios-arrow-forward"></i></a></p>
@@ -452,15 +319,15 @@
           </div>
           <div class="col-md-4 d-flex ftco-animate">
           	<div class="blog-entry align-self-stretch">
-              <a href="blog-single.html" class="block-20" style="background-image: url('images/event-3.jpg');">
+              <a href="blog-single.html" class="block-20" style="background-image: url({{ asset('images/kegiatan/job_matching.jpg') }});">
               </a>
               <div class="text p-4 d-block">
               	<div class="meta mb-3">
-                  <div><a href="#">Sep. 10, 2018</a></div>
+                  <div><a href="#">10 September 2019</a></div>
                   <div><a href="#">Admin</a></div>
                   <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
                 </div>
-                <h3 class="heading mb-4"><a href="#">Intern Bootcamp Meetup 2018</a></h3>
+                <h3 class="heading mb-4"><a href="#">SMKN 2 Bandung menekan jumlah pengangguran melalui event Job Matching</a></h3>
                 <p class="time-loc"><span class="mr-2"><i class="icon-clock-o"></i> 10:30AM-03:30PM</span> <span><i class="icon-map-o"></i> Venue Main Campus</span></p>
                 <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
                 <p><a href="event.html">Join Event <i class="ion-ios-arrow-forward"></i></a></p>
